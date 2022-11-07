4b09c3faf8c2910a1e708cfbe4254a59e9a1022e
Daniel Morris <daniellmorris@gmail.com>
2022-11-07 15:40:19 -0600

    Add message output to game simulator

--- simulator/src/utils/udp.ts
+++ simulator/src/utils/udp.ts
@@ -2,6 +2,7 @@ import dgram from 'dgram';
 import { Buffer } from 'buffer';
 
 export async function sendUdpMessage(data: any) {
+  console.log('sendUdpMessage', JSON.stringify(data, null, 2));
   const message = Buffer.from(JSON.stringify(data));
   const client = dgram.createSocket('udp4');
 
